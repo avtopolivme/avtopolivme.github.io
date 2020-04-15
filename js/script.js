fc59b3d782f53ae67d0626f712bd9b46cdb5a33a
Aleksei Litivshko <uthhar@yandex.ru>
2020-04-15 14:46:19 +0700

    up button

--- js/script.js
+++ js/script.js
@@ -3,16 +3,34 @@ $(document).ready(function()
   $(document).on('click', '.js-burger', function(e)
   {
     $(this).hide();
-    $('.js-close').fadeToggle('slow','linear');
-    $('.js-mobile-menu').fadeToggle('slow','linear');
-    $('.js-mobile-menu-overlay').fadeToggle('slow','linear');
+    $('.js-close').fadeToggle('slow', 'linear');
+    $('.js-mobile-menu').fadeToggle('slow', 'linear');
+    $('.js-mobile-menu-overlay').fadeToggle('slow', 'linear');
   });
 
   $(document).on('click', '.js-close', function(e)
   {
     $(this).hide();
-    $('.js-mobile-menu').fadeToggle('slow','linear');
-    $('.js-mobile-menu-overlay').fadeToggle('slow','linear');
-    $('.js-burger').fadeToggle('slow','linear');
+    $('.js-mobile-menu').fadeToggle('slow', 'linear');
+    $('.js-mobile-menu-overlay').fadeToggle('slow', 'linear');
+    $('.js-burger').fadeToggle('slow', 'linear');
+  });
+
+  $(window).scroll(function()
+  {
+    if ($(window).scrollTop() >= 400) {
+      $('.js-up').fadeIn('slow', 'linear');
+    }
+
+    if ($(window).scrollTop() < 400) {
+      $('.js-up').fadeOut('slow', 'linear');
+    }
+  });
+
+  $(document).on('click', '.js-up', function(e)
+  {
+    e.preventDefault();
+
+    $('html, body').animate({scrollTop: 0},500);
   });
 });
